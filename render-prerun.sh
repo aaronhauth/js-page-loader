59112c6baa8da49a12b5f7c559cf1e4c9dad1538
Aaron Hauth <aaronhauth@gmail.com>
2025-08-02 11:52:27 -0500

    add pre-run script

---
+++ render-prerun.sh
@@ -0,0 +1,2 @@
+apt-get update
+apt-get install -y libnspr4 libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libgbm-dev libasound2 libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 libgtk-3-0
